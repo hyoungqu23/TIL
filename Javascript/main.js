9e22569fb761f47d70a60165c863f3a661fe812c
mini_VANTA <76457116+hyoungqu23@users.noreply.github.com>
2021-09-26 22:48:03 +0900

    NKLCB_FE_Online | JS Chap. 02 I/O Example

---
+++ Javascript/main.js
@@ -0,0 +1,17 @@
+// 개요
+// console.log('Hello, world!');
+
+// (예제) 입력과 출력
+const fs = require("fs");
+const input = fs.readFileSync("./Javascript/input.txt", "utf8");
+
+console.log(input);
+
+// (예제) 코드 구성
+let hi = "hello";
+let HI = "HELLO";
+let 안녕 = "하세요";
+
+console.log(hi);
+console.log(HI);
+console.log(안녕);
