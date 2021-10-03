f6e4874541ba9f3eebfca75dc814b9bf9ad9bb26
mini_VANTA <76457116+hyoungqu23@users.noreply.github.com>
2021-10-03 12:02:30 +0900

    NKLCB_FE_Online | JS Chap. 09 Javascript Conditionals Examples

--- Javascript/main.js
+++ Javascript/main.js
@@ -382,11 +382,11 @@ scope();
 console.log(x);
 console.log(y);
 
-// ========== (예제) Conditionals ==========
+// ========== (예제) Conditionals - if ==========
 let apple_price = 9;
 
 if (apple_price >= 10) {
-  console.log("very expensive");
+  console.log("Very Expensive");
 } else if ( apple_price < 5 ) {
   console.log("Very Cheap");
 } else {
@@ -397,4 +397,116 @@ let pet_age = 10;
 if (pet_age < 10) 
   console.log('Young');
 else
-  console.log("old");
+  console.log("Old");
+
+// ========== (예제) Conditionals - Conditional (ternary) operator ==========
+let user_age = 20;
+
+if (user_age < 19) {
+  msg = "The user is not an adult."
+} else {
+  msg = "The user is an adult."
+}
+
+console.log(msg);
+
+msg_dblchecked = age < 19 ? "Not adult": "Adult";
+console.log(msg_dblchecked);
+
+// ========== (예제) Conditionals - switch ==========
+let day_number = 1;
+let day;
+
+switch (day_number) {
+  case 0:
+    day = "Sunday"; break;
+  case 1:
+    day = "Monday"; break;
+  case 2:
+    day = "Tuesday"; break;
+  case 3:
+    day = "Wednesday"; break;
+  case 4:
+    day = "Thursday"; break;
+  case 5:
+    day = "Friday"; break;
+  case 6:
+    day = "Saturday"; break;
+  default:
+    day = "error"; break;
+}
+
+console.log(day);
+
+let browser = 'Chrome';
+
+switch (browser) {
+  case "Explorer":
+    msg = "ActiveX installation required.";
+    break;
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    msg = "Supported Browsers.";
+    break;
+  default:
+    msg = "Unsupported Browsers.";
+    break;
+}
+
+console.log(msg);
+
+// ========== (연습 문제 #1) ==========
+const day_quiz = 3;
+let weekday = '';
+
+switch (day_quiz) {
+  case 1:
+    weekday = "월요일";
+    break;
+  case 2:
+    weekday = "화요일";
+    break;
+  case 3:
+    weekday = "수요일";
+    break;
+  case 4:
+    weekday = "목요일";
+    break;
+  case 5:
+    weekday = "금요일";
+    break;
+  case 6:
+    weekday = "토요일";
+    break;
+  case 7:
+    weekday = "일요일";
+    break;
+  default:
+    weekday = "잘못된 값입니다. 숫자를 입력해주세요.";
+    break;
+}
+
+console.log(weekday);
+
+// ========== (예제) Loops ==========
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+for (let i = 10; i < 3; i++) {
+  console.log(i);
+}
+
+let num_loop = 0;
+for ( ; num_loop < 2; ) {
+  console.log(num_loop);
+  num_loop++;
+}
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    console.log(`${i} + ${j} = ${i + j}`);
+  }
+}
