7d60054dc6eb1886ac06653981862422e3793511
mini_VANTA <76457116+hyoungqu23@users.noreply.github.com>
2022-04-01 11:24:03 +0900

    20220401 Modify App.js

---
+++ React/react-for-beginners/src/App.js
@@ -0,0 +1,14 @@
+  // import logo from './logo.svg';
+  // import './App.css';   불필요한 코드 제거
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome back!!!</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
