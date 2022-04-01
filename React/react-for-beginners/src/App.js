c7eef69ee33b9f124b62b6055b8b235dd9595062
mini_VANTA <76457116+hyoungqu23@users.noreply.github.com>
2022-04-01 23:10:20 +0900

    20220401 Style Modules for CRA

--- React/react-for-beginners/src/App.js
+++ React/react-for-beginners/src/App.js
@@ -1,12 +1,13 @@
-  // import logo from './logo.svg';
-  // import './App.css';   불필요한 코드 제거
+  // import logo from './logo.svg';   불필요한 코드 제거
+  // import './App.css';
 import Button from './Button';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
-      <h1>Welcome back!!!</h1>
-      <Button />
+      <h1 className={styles.title}>Welcome back!!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
