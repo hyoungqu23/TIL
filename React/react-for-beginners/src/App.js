457449ea9e37ee6556ca8ea0f86af824a380cca3
mini_VANTA <76457116+hyoungqu23@users.noreply.github.com>
2022-04-02 22:32:29 +0900

    20220402 CRA

--- React/react-for-beginners/src/App.js
+++ React/react-for-beginners/src/App.js
@@ -2,12 +2,18 @@
   // import './App.css';
 import Button from './Button';
 import styles from './App.module.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  }
   return (
     <div>
       <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <Button text={"Continue"} onClick={onClick} />
+      <h3>{counter}</h3>
     </div>
   );
 }
--- React/react-for-beginners/src/index.js
+++ React/react-for-beginners/src/index.js
@@ -4,12 +4,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
   // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+// React.js 18 업데이트 이후 ReactDOM.render is no longer supported in React 18 에러 해결
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
+
+// [React.js 17]
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
   // // If you want to start measuring performance in your app, pass a function
   // // to log results (for example: reportWebVitals(console.log))
