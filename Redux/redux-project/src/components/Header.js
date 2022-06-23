d0a3e1427ebb9efdfa28845dd8220e04c60da736
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-24 02:48:14 +0900

    [lec] Header Component
    Header Component에서 로그아웃 시 State 변경 로직 추가 및 로그인 여부에 따른 UI 변경 기능 추가

--- Redux/redux-project/src/components/Header.js
+++ Redux/redux-project/src/components/Header.js
@@ -1,21 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
+
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const handleLogoutButton = (event) => {
+    event.preventDefault();
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {isAuthenticated && (
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={handleLogoutButton}>Logout</button>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
