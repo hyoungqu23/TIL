362d99c1da773ac9a5ef56c3a7311ba39d54bb22
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-09 23:49:36 +0900

    cart State가 변경될 때마다 Fetch하는 비동기 통신 로직 구현

--- Redux/deeper-redux/src/App.js
+++ Redux/deeper-redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,20 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.cartUi.isCartVisible);
+
+  // 비동기 처리
+  const cart = useSelector((state) => state.cart);
+
+  // cart State가 변경될 때마다 Fetch하는 비동기 통신 로직 구현
+  useEffect(() => {
+    fetch(
+      'https://react-app-283cb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      },
+    );
+  }, [cart]);
 
   return (
     <Layout>
