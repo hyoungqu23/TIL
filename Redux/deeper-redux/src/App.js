efda12605ddb05915d654d48629828816c73e6cf
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-10 20:16:20 +0900

    RTK로 비동기 처리하기

--- Redux/deeper-redux/src/App.js
+++ Redux/deeper-redux/src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { cartUiActions } from './store/cartUi-slice';
+// import { cartUiActions } from './store/cartUi-slice';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
+import { sendCartData, getCartData } from './store/cart-slice';
 
 // App Component 외부에 변수를 설정해 해당 파일이 처음으로 파싱될 때 초기화되고, Component가 다시 렌더링된다 하더라도 다시 초기화되지 않는다.
 let isInitial = true;
@@ -18,55 +19,62 @@ function App() {
   // 비동기 처리
   const cart = useSelector((state) => state.cart);
 
+  // Get Cart Data from DB at First Render
+  // 다만 이때 cart가 변경되며 아래의 useEffect의 dependencies가 변경되어 실행되게 된다.
+  useEffect(() => {
+    dispatch(getCartData());
+  }, [dispatch]);
+
   // cart State가 변경될 때마다 Fetch하는 비동기 통신 로직 구현
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        cartUiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending Cart Data!',
-        }),
-      );
-
-      const response = await fetch(
-        'https://react-app-283cb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        },
-      );
-
-      // 모든 종류의 에러를 처리하지 않음. 단지 응답 상태가 200-299 사이가 아닌 경우에만 에러 처리
-      if (!response.ok) {
-        throw new Error('Sending Cart Data failed');
-      }
-
-      dispatch(
-        cartUiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sending Cart Data Successfully!',
-        }),
-      );
-    };
-
+    // const sendCartData = async () => {
+    //   dispatch(
+    //     cartUiActions.showNotification({
+    //       status: 'pending',
+    //       title: 'Sending...',
+    //       message: 'Sending Cart Data!',
+    //     }),
+    //   );
+    //   const response = await fetch(
+    //     'https://react-app-283cb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+    //     {
+    //       method: 'PUT',
+    //       body: JSON.stringify(cart),
+    //     },
+    //   );
+    //   // 모든 종류의 에러를 처리하지 않음. 단지 응답 상태가 200-299 사이가 아닌 경우에만 에러 처리
+    //   if (!response.ok) {
+    //     throw new Error('Sending Cart Data failed');
+    //   }
+    //   dispatch(
+    //     cartUiActions.showNotification({
+    //       status: 'success',
+    //       title: 'Success!',
+    //       message: 'Sending Cart Data Successfully!',
+    //     }),
+    //   );
+    // };
     // 앱이 실행될 때 장바구니 데이터를 서버에 전달하지 않는다.
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    // catch 구문으로 발생할 수 있는 에러를 포착하여 dispatch 함수를 실행할 수 있다.
-    sendCartData().catch((error) => {
-      dispatch(
-        cartUiActions.showNotification({
-          status: 'error',
-          title: 'Error...',
-          message: 'Sending Cart Data Failed...',
-        }),
-      );
-    });
+    // // catch 구문으로 발생할 수 있는 에러를 포착하여 dispatch 함수를 실행할 수 있다.
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     cartUiActions.showNotification({
+    //       status: 'error',
+    //       title: 'Error...',
+    //       message: 'Sending Cart Data Failed...',
+    //     }),
+    //   );
+    // });
+
+    // RTK는 Action Creator 함수를 반환하는 함수도 Dispatch의 인수로 받는 것을 허용하고, 이러한 함수를 자동으로 실행해주며, dispatch 중첩된 내부 함수의 인자로 전달해 Dispatch할 수 있도록 해준다.
+    if (cart.isChange) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- Redux/deeper-redux/src/store/cart-slice.js
+++ Redux/deeper-redux/src/store/cart-slice.js
@@ -1,16 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { cartUiActions } from './cartUi-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
+    isChange: false,
   },
+
   reducers: {
+    // Action Creator 함수
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
+    // Action Creator 함수
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.isChange = true;
 
       if (!existingItem) {
         state.items.push({
@@ -26,10 +37,12 @@ const cartSlice = createSlice({
       }
     },
 
+    // Action Creator 함수
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.isChange = true;
 
       if (existingItem.quantity === 1) {
         // 배열에서 제거
@@ -41,6 +54,101 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// Action Creator 함수를 반환하는 함수
+// Get Cart Data from DB and Set Cart Data
+export const getCartData = () => {
+  return async (dispatch) => {
+    const getData = async () => {
+      const response = await fetch(
+        'https://react-app-283cb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        {
+          method: 'GET',
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data.');
+      }
+      console.log('response', response);
+      const data = await response.json();
+      console.log('data', data);
+      return data;
+    };
+
+    try {
+      const cartData = await getData();
+      console.log('cartData', cartData);
+      // Setting Cart Data from DB to Frontend
+      dispatch(
+        cartActions.replaceCart({
+          // 모든 아이템을 Cart에서 삭제하면, DB에 items field가 존재하지 않게 되어 다시 추가하는 경우에 에러가 발생한다.
+          // 따라서 빈 배열 처리를 해주어야 한다.
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        }),
+      );
+    } catch (error) {
+      dispatch(
+        cartUiActions.showNotification({
+          status: 'error',
+          title: 'Error...',
+          message: 'Getting Cart Data Failed...',
+        }),
+      );
+    }
+  };
+};
+
+// Action Creator 함수를 반환하는 함수
+// Send Cart Data to DB
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    // Dispatch 함수 실행
+    dispatch(
+      cartUiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending Cart Data!',
+      }),
+    );
+
+    // 비동기 코드, 부수 효과 코드 실행 가능
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://react-app-283cb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        },
+      );
+
+      // 모든 종류의 에러를 처리하지 않음. 단지 응답 상태가 200-299 사이가 아닌 경우에만 에러 처리
+      if (!response.ok) {
+        throw new Error('Sending Cart Data failed');
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        cartUiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sending Cart Data Successfully!',
+        }),
+      );
+    } catch (error) {
+      dispatch(
+        cartUiActions.showNotification({
+          status: 'error',
+          title: 'Error...',
+          message: 'Sending Cart Data Failed...',
+        }),
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
