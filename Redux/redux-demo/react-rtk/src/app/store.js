acbc9b0bbd26a0ed4a99eb4195ead8edad8ffeab
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-20 13:40:50 +0900

    import-export 수정

--- Redux/redux-demo/react-rtk/src/app/store.js
+++ Redux/redux-demo/react-rtk/src/app/store.js
@@ -5,9 +5,9 @@ import { configureStore } from '@reduxjs/toolkit';
 // const logger = reduxLogger.createLogger();
 
 // Import Reducers
-const snackReducer = require('../features/snack/snackSlice');
-const coffeeReducer = require('../features/coffee/coffeeSlice');
-const userReducer = require('../features/users/userSlice');
+import snackReducer from '../features/snack/snackSlice';
+import coffeeReducer from '../features/coffee/coffeeSlice';
+import userReducer from '../features/users/userSlice';
 
 // Declare store
 const store = configureStore({
@@ -16,7 +16,7 @@ const store = configureStore({
     coffee: coffeeReducer,
     user: userReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
