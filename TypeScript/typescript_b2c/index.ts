4b005c8c9c1bf75cf1df07bc0cac136e0acb1e76
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-31 11:56:49 +0900

    Type Alias, Type of Functions, Optional Type

--- TypeScript/typescript_b2c/index.ts
+++ TypeScript/typescript_b2c/index.ts
@@ -5,19 +5,57 @@ let a = 'hello';
 a = 'bye';
 
 // string to number: Error
-// Error TS2322: 'number' 형식은 'string' 형식에 할당할 수 없습니다.
+// Error: 'number' 형식은 'string' 형식에 할당할 수 없습니다.
 a = 123;
 
-let player = {
+let player1 = {
   name: 'John',
   age: 22,
 };
 
-player.name = 'Jane';
-player.age = 44;
+player1.name = 'Jane';
+player1.age = 44;
 
 // Type specification
 let b: boolean = true;
 
 let c: number[] = [1, 2, 3];
 c.push(4);
+
+// optional type
+const player2: { name: string; age?: number } = { name: 'John' };
+
+// const player2: {
+//   name: string;
+//   age?: number | undefined;
+// }
+
+if (player2.age && player2.age < 10) {
+  // Error: 'undefined'인 것 같습니다.
+  console.log(player2.age);
+}
+
+// Type Alias
+type PlayerType = {
+  name: string;
+  age?: number;
+};
+
+const player3: PlayerType = { name: 'John' };
+const player4: PlayerType = { name: 'James', age: 30 };
+const player5: PlayerType = { name: 'Smith', age: 22 };
+
+// function Type
+function playerMaker(name: string): PlayerType {
+  return {
+    name,
+  };
+}
+
+const james = playerMaker('james');
+james.age = 33;
+
+// Arrow Function Type
+const playerArrowMaker = (name: string): PlayerType => ({
+  name,
+});
