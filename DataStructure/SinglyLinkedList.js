d2be8882c7b9132de546fd6f0e786200bd5ce556
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-28 16:47:34 +0900

    단일 연결 리스트 push 메서드

--- DataStructure/SinglyLinkedList.js
+++ DataStructure/SinglyLinkedList.js
@@ -6,6 +6,23 @@ export class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push = (value) => {
+    // 새로운 노드 생성
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  };
 }
 
 let singlyLinkedList = new SinglyLinkedList();
